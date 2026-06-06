ccb19e68df234421d69fc03e392642d6fe3fb510
WafaeyKhaled1 <wafaeykhaled732@gmail.com>
2026-06-06 21:30:30 +0300

    Implement CRUD functionality for posts and add users controller and model

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :name, :dob, :email, :phone_number, presence: true
+  validates :email, uniqueness: true,
+                    format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+end
