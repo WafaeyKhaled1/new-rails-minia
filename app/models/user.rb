eabed8fec32850f60e921f54949bec6530abac29
WafaeyKhaled1 <wafaeykhaled732@gmail.com>
2026-06-07 22:04:02 +0300

    Create a creator association in the User model that links to the Post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+  has_many :created_posts, class_name: "Post", foreign_key: :user_id, dependent: :destroy
 
   validates :name, :dob, :email, :phone_number, presence: true
   validates :email, uniqueness: true,
