73751a5f4d70ff619d4bf4f4a9fcd431f0391f4a
WafaeyKhaled1 <wafaeykhaled732@gmail.com>
2026-06-07 22:03:21 +0300

    Create the application using scaffolding

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+
   validates :name, :dob, :email, :phone_number, presence: true
   validates :email, uniqueness: true,
                     format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
